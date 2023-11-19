0170f2fab17f3787b8a62e28d8c28501c3ccf302
Kunalpal216 <kunalpal2115@gmail.com>
2023-11-19 23:49:19 +0530

    navbar bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ export default function Navbar() {
                             <Link to="/" class="block py-2 px-2 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
                         </li>
                         <li>
-                            <Link to="/comic-strip" class="block py-2 x-2 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create</Link>
+                            <Link to="/comic-strip" class="block py-2 x-2 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create</Link>
                         </li>
                     </ul>
                 </div>
